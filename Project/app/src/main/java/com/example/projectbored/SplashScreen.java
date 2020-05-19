4c6f74d00ef660d6dca8382bed5bc4b6348027aa
Szabó Bence <f5pn1y@inf.elte.hu>
2020-05-19 22:32:41 +0200

    Fixed splash screen, reversed order of yes/no buttons on reset database popup

--- Project/app/src/main/java/com/example/projectbored/SplashScreen.java
+++ Project/app/src/main/java/com/example/projectbored/SplashScreen.java
@@ -12,7 +12,6 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
         try {
             sleep(1000);
         } catch (InterruptedException e) {
